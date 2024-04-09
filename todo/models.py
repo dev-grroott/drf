7f6e1043370591c847be93b94623a6fcc61f5017
Gokulnath S <sgokulnathmech@gmail.com>
2024-04-09 22:26:20 +0530

    Auth implementation

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 class Task(models.Model):
     name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
 from todo.models import Task
 from todo.serializer import TaskSerializer
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 def home(request):
     pass
 
 
+class CustomTokenAuthentication(TokenAuthentication):
+    keyword = "Bearer"
+
 class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    http_method_names = ["get", "post", "put", "delete"]
-
+    authentication_classes = [CustomTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    http_method_names = ["get", "post", "put", "delete", "patch"]
